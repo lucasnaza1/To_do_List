dc6b570ac47bfd26397f251305c4f4779449809f
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-06-30 15:27:25 -0300

    function create new task

--- Back-end/src/models/taskModel.js
+++ Back-end/src/models/taskModel.js
@@ -5,6 +5,23 @@ const getAll = async () => {
   return tasks;
 };
 
+const addTask = async (task) => {
+  const [task] = task;
+
+  const query = "INSERT INTO tasks(task,status,created_at) VALUES(?,?,?)";
+
+  const dateUTC = new DataTransfer(Date.now()).toUTCString();
+
+  const [addedTask] = await connection.execute(query, [
+    task,
+    "pendente",
+    dateUTC,
+  ]);
+
+  return addedTask;
+};
+
 module.exports = {
   getAll,
+  addTask,
 };
