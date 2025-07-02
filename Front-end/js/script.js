f818af6ecc485a134f55693f7da2021e3258963e
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-07-02 15:17:45 -0300

    Update script.js

--- Front-end/js/script.js
+++ Front-end/js/script.js
@@ -1,7 +1,5 @@
 const fetchTasks = async () => {
   const response = await fetch("http://localhost:3333/tasks");
-
-  console.log(response);
+  const tasks = await response.json();
+  return tasks;
 };
-
-fetchTasks();
