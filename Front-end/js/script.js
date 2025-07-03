6b03f0c30f4b62ae81038193cc3be6abac3a5358
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-07-03 16:25:56 -0300

    creating elements on table

--- Front-end/js/script.js
+++ Front-end/js/script.js
@@ -5,17 +5,31 @@ const fetchTasks = async () => {
   const tasks = await response.json();
   return tasks;
 };
-// Função criar elemento que será chamada na hora de criar as tags da tabela
-const createElement = (tag, innerText = "") => {
+
+const createElement = (tag, innerText = "", innerHTML = "") => {
   const elemento = document.createElement(tag);
-  elemento.innerText = innerText;
+  if (innerText) {
+    elemento.innerText = innerText;
+  }
+  if (innerHTML) {
+    elemento.innerHTML = innerHTML;
+  }
 
   return elemento;
 };
-// Teste
+
+const createSelect = () => {
+  const options =
+    "<option value='Pendente'>Pendente</option><option value='Em Progresso'>Em progresso</option><option value='Concluido'>Concluido</option>";
+
+  const select = createElement("select", "", options);
+
+  return select;
+};
+
 const task = {
   id: 1,
-  title: "Estudar JavaScript",
+  title: "Estudar React",
   created_at: " 02 Julho de 2025 18:06",
   status: "pendente",
 };
@@ -25,8 +39,35 @@ const createRow = (task) => {
 
   const tr = createElement("tr");
   const tdTitle = createElement("td", title);
+  const tdcreactedAt = createElement("td", created_at);
+  const tdStatus = createElement("td");
+  const tdActions = createElement("td");
+
+  const select = createSelect();
+
+  const editButton = createElement(
+    "button",
+    "",
+    "<span class='material-symbols-outlined'> edit </span>"
+  );
+  const deleteButton = createElement(
+    "button",
+    "",
+    "<span class='material-symbols-outlined'> delete </span>"
+  );
+
+  editButton.classList.add("btn-action");
+  deleteButton.classList.add("btn-action");
+
+  tdActions.appendChild(editButton);
+  tdActions.appendChild(deleteButton);
+
+  tdStatus.appendChild(select);
 
   tr.appendChild(tdTitle);
+  tr.appendChild(tdcreactedAt);
+  tr.appendChild(tdStatus);
+  tr.appendChild(tdActions);
 
   tBody.appendChild(tr);
 };
