114eb3ef1ec42c6fd233e9042ac42db94b970a4f
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-07-04 13:44:48 -0300

    documenting and creating new functions

--- Front-end/js/script.js
+++ Front-end/js/script.js
@@ -1,11 +1,35 @@
 const tBody = document.querySelector("tbody");
-
+const addForm = document.querySelector(".add-form");
+const inputTask = document.querySelector(".input-task");
+// Função de busca das tasks
 const fetchTasks = async () => {
   const response = await fetch("http://localhost:3333/tasks");
   const tasks = await response.json();
   return tasks;
 };
+// Função adicionar task
+const addTask = async (event) => {
+  event.preventDefault();
 
+  const task = { title: inputTask.value };
+
+  await fetch("http://localhost:3333/tasks", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(task),
+  });
+
+  loadTasks();
+  inputTask.value = "";
+};
+// Função para formatar a data
+const formatDate = (dateUTC) => {
+  const options = { dateStyle: "long", timeStyle: "short" };
+  const date = new Date(dateUTC).toLocaleString("pt-br", options);
+
+  return date;
+};
+// Função que cria elementos HTML
 const createElement = (tag, innerText = "", innerHTML = "") => {
   const elemento = document.createElement(tag);
   if (innerText) {
@@ -17,7 +41,7 @@ const createElement = (tag, innerText = "", innerHTML = "") => {
 
   return elemento;
 };
-
+// Função que gera botões de edit e delete
 const createSelect = () => {
   const options =
     "<option value='Pendente'>Pendente</option><option value='Em Progresso'>Em progresso</option><option value='Concluido'>Concluido</option>";
@@ -26,20 +50,20 @@ const createSelect = () => {
 
   return select;
 };
-
+// Task de teste
 const task = {
   id: 1,
   title: "Estudar React",
   created_at: " 02 Julho de 2025 18:06",
   status: "pendente",
 };
-
+// Função que cria a linha da tabela
 const createRow = (task) => {
   const { id, title, created_at, status } = task;
 
   const tr = createElement("tr");
   const tdTitle = createElement("td", title);
-  const tdcreactedAt = createElement("td", created_at);
+  const tdcreactedAt = createElement("td", formatDate(created_at));
   const tdStatus = createElement("td");
   const tdActions = createElement("td");
 
@@ -69,7 +93,19 @@ const createRow = (task) => {
   tr.appendChild(tdStatus);
   tr.appendChild(tdActions);
 
-  tBody.appendChild(tr);
+  return tr;
+};
+// Função que irá buscar as tasks no banco de dados e carregar na tela
+const loadTasks = async () => {
+  const tasks = await fetchTasks();
+
+  tBody.innerHTML = "";
+
+  tasks.forEach((task) => {
+    const tr = createRow(task);
+    tBody.appendChild(tr);
+  });
 };
 
-createRow(task);
+addForm.addEventListener("submit", addTask);
+loadTasks();
